4267fb0e9094f160c695dad55065bb22c6294314
root <root@DESKTOP-NLHM8LU.localdomain>
2024-07-23 23:29:23 +0530

    my-app changed

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="my-app",
+    name="intro_devops",
     version="0.1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
